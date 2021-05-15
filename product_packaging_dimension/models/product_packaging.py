d7b269d058671a7595062e5e63d0c8f8c4355509
Sébastien Gouverneur <76954991+captivea-sgo@users.noreply.github.com>
2021-05-15 10:38:35 -0400

    Product Dimensions update

--- product_packaging_dimension/models/product_packaging.py
+++ product_packaging_dimension/models/product_packaging.py
@@ -6,23 +6,23 @@ from odoo import api, fields, models
 class ProductPackaging(models.Model):
     _inherit = "product.packaging"
 
-    max_weight = fields.Float("Weight (kg)")
+    max_weight = fields.Float("Weight (lb)",digits=(8,4),)
     # lngth IS NOT A TYPO https://github.com/odoo/odoo/issues/41353
-    lngth = fields.Integer("Length (mm)", help="length in millimeters")
+    lngth = fields.Integer("Length (in)", help="length in inches
     # Although it feels weird to use Integer in millimeters, we use Int to
     # override the fields from delivery module instead of defining new ones
-    width = fields.Integer("Width (mm)", help="width in millimeters")
-    height = fields.Integer("Height (mm)", help="height in millimeters")
+    width = fields.Integer("Width (in)", help="width in inches
+    height = fields.Integer("Height (in)", help="height in inches")
     volume = fields.Float(
-        "Volume (m³)",
+        "Volume (ft³)",
         digits=(8, 4),
         compute="_compute_volume",
         readonly=True,
         store=False,
-        help="volume in cubic meters",
+        help="volume in cubic feet",
     )
 
     @api.depends("lngth", "width", "height")
     def _compute_volume(self):
         for pack in self:
-            pack.volume = (pack.lngth * pack.width * pack.height) / 1000.0 ** 3
+            pack.volume = (pack.lngth * pack.width * pack.height) / 1728.0 ** 3
